d208257a4fc12cedc5c7942494b9a44c29a8918f
furkan harmancı <rocklee34@hotmail.com>
2023-09-19 12:09:34 +0300

    build: target and compile sdk are changed to 34 🛠

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.jsonplaceholderretrofitkotlin"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.jsonplaceholderretrofitkotlin"
         minSdk = 33
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
