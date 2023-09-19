cc460db71c4cf572b92bab4240cdb562931194a5
furkan harmancı <rocklee34@hotmail.com>
2023-09-19 14:55:15 +0300

    feat: Rxjava, composite are added :sparkles:

--- app/src/main/java/com/example/jsonplaceholderretrofitkotlin/MainActivity.kt
+++ app/src/main/java/com/example/jsonplaceholderretrofitkotlin/MainActivity.kt
@@ -2,55 +2,75 @@ package com.example.jsonplaceholderretrofitkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.jsonplaceholderretrofitkotlin.adapter.ApiAdapter
+import com.example.jsonplaceholderretrofitkotlin.databinding.ActivityMainBinding
 import com.example.jsonplaceholderretrofitkotlin.model.ApiModel
 import com.example.jsonplaceholderretrofitkotlin.service.ApiService
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
-class MainActivity : AppCompatActivity() {
-    private val BASE_URL = "https://jsonplaceholder.typicode.com/"
-    private var apiModels : List<ApiModel>? = null
+class MainActivity : AppCompatActivity(), ApiAdapter.Listener {
+    private val baseUrl = "https://raw.githubusercontent.com/atilsamancioglu/"
+    private var apiModels : ArrayList<ApiModel>? = null
+    private var compositeDisposable : CompositeDisposable? = null
+    private lateinit var recyclerView : RecyclerView
+    private lateinit var apiAdapter: ApiAdapter
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        compositeDisposable = CompositeDisposable()
+
+        val layout : RecyclerView.LayoutManager = LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = layout
 
         getData()
     }
 
     private fun getData() {
         val retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL)
+            .baseUrl(baseUrl)
             .addConverterFactory(GsonConverterFactory.create())
-            .build()
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .build().create(ApiService::class.java)
 
-        val getDataCall = retrofit.create(ApiService::class.java)
-        val service = getDataCall.getAlbumsData()
+        val getAlbums = retrofit.getAlbumsData()
 
-        service.enqueue(object : Callback<List<ApiModel>> {
-            override fun onResponse(
-                call: Call<List<ApiModel>>,
-                response: Response<List<ApiModel>>
-            ) {
-                if (response.isSuccessful) {
-                    response.body()?.let {
-                        apiModels = ArrayList(it)
+        compositeDisposable?.add(
+            getAlbums
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(this::handleResponse)
+        )
+    }
 
-                        for (apiModel : ApiModel in apiModels!!) {
-                            println(apiModel.id)
-                            println(apiModel.title)
-                        }
-                    }
-                }
-            }
+    private fun handleResponse(apiList : List<ApiModel>) {
+        apiModels = ArrayList(apiList)
 
-            override fun onFailure(call: Call<List<ApiModel>>, t: Throwable) {
-                t.printStackTrace()
-            }
+        apiModels?.let {
+            apiAdapter = ApiAdapter(it, this@MainActivity)
+        }
 
-        })
+        binding.recyclerView.adapter = apiAdapter
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable?.clear()
+    }
+
+    override fun onItemClick(apiModel: ApiModel) {
+        Toast.makeText(this, "Currency: ${apiModel.currency}\nPrice: ${apiModel.price}", Toast.LENGTH_LONG).show()
     }
 }
